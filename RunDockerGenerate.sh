cf98ccc2b49ab2cfe80b573a85c6b1989b762f48
Gerhard Erwin Harmsen <60874312+GerhardHarmsen@users.noreply.github.com>
2020-08-05 10:17:24 +0200

    Update RunDockerGenerate.sh
    Added the additional processes we wish to study into the docker run bash

--- RunDockerGenerate.sh
+++ RunDockerGenerate.sh
@@ -1,5 +1,7 @@
 DockerName=MADGraphDocker
 
+sudo docker
+
 cd 
 cd Documents
 mkdir DockerOutput 
@@ -11,36 +13,177 @@ echo $OUTPUT
 mkdir MADGraphScripts 
 cd $_
 
-/bin/cat <<EOM > ProtonProtonToTopTopBar
+sudo docker run -dit --rm --name $DockerName -v $OUTPUT/models:/var/UFO_models -v $OUTPUT/outputs:/var/MG_outputs hfukuda/madgraph
 
-import model sm/
-define l+ = e+ mu+ ta+
+######################## Setup directories for easy saving of results ############
+############## Variables for the scripts #########################################
+BACKGROUNDRUNS=20
+SIGNALRUNS=5
+EVENTSPERRUN=10000
+############## Variables for the scripts #########################################
+############## Background events #################################################
+FILE="PPtoTopTopBar"
+
+/bin/cat <<EOM>$FILE
+####################################################################################
+#####File Generates the following
+##### Proton Proton to top top-bar with two jets
+##### With $((BACKGROUNDRUNS * EVENTSPERRUN)) events
+##### xqcut value of 30
+####################################################################################
+
+
+define l+ = e+ mu+ ta+ 
 define l- = e- mu- ta-
-generate p p > t t~
-add process p p > t t~ j
-add process p p > t t~ j j
-output pp_ttbar_ll_13TeV_xqcut_45
-launch pp_ttbar_ll_13TeV_xqcut_45 -i
-multi_run 2
+generate p p > t t~ @0
+add process p p > t t~ j @1
+add process p p > t t~ j j @2
+output ${FILE}
+launch ${FILE} -i
+multi_run ${BACKGROUNDRUNS}
 1
 4
 0
-decay t > w+ b , w+ > l+ vl
-decay t~ > w- b~ , w- > l- vl~
 set ebeam = 6500
-set nevents = 10000
+set nevents = ${EVENTSPERRUN}
 set ickkw = 1
+decay t > w+ b, w+ > l+ vl
+decay t~ > w- b~, w- > l- vl~
 set xqcut = 30
 set etaj = 5
 0
 EOM
 
-sudo docker run -dit --rm --name $DockerName -v $OUTPUT/models:/var/UFO_models -v $OUTPUT/outputs:/var/MG_outputs hfukuda/madgraph
+sudo docker cp $OUTPUT/MADGraphScripts/$FILE MADGraphDocker:/var/MG_outputs
 
-sudo docker cp $OUTPUT/MADGraphScripts/ProtonProtonToTopTopBar MADGraphDocker:/var/MG_outputs
+sudo docker exec -it MADGraphDocker /home/hep/MG5_aMC_v2_6_3_2/bin/mg5_aMC  $FILE
+#############################################################
+### End of process generator  ###############################
+#############################################################
+	
+FILE="PP_W_LeptonNeutrino"
+
+/bin/cat <<EOM>$FILE
+####################################################################################
+##### File Generates the following
+##### Proton Proton to lepton neutrino pairs with two jets (These smuons decay to muons) 
+##### With $((BACKGROUNDRUNS * EVENTSPERRUN)) events
+##### xqcut value of 25
+####################################################################################
 
 
-sudo docker exec -it MADGraphDocker /home/hep/MG5_aMC_v2_6_3_2/bin/mg5_aMC  ProtonProtonToTopTopBar
+define l+ = e+ mu+ ta+ 
+define l- = e- mu- ta-
+define ll = l+ l-
+define vv = vl vl~
+generate p p > ll vv @0
+add process p p > ll vv j @1
+add process p p > ll vv j j  @2
+output ${FILE}
+launch ${FILE} -i
+multi_run ${BACKGROUNDRUNS}
+1
+0
+set ebeam = 6500
+set nevents = ${EVENTSPERRUN}
+set ickkw = 1
+set xqcut = 25
+set etaj = 5
+0
+EOM
+
+cd $WORKINGPATH
+
+sudo docker cp $OUTPUT/MADGraphScripts/$FILE MADGraphDocker:/var/MG_outputs
+
+sudo docker exec -it MADGraphDocker /home/hep/MG5_aMC_v2_6_3_2/bin/mg5_aMC  $FILE
+#############################################################
+### End of process generator  ###############################
+#############################################################
+
+FILE="PP_WW_lvl"
+
+/bin/cat <<EOM>$FILE
+####################################################################################
+##### File Generates the following
+##### Proton Proton to lepton neutrino pairs with two jets (These smuons decay to muons) 
+##### With $((BACKGROUNDRUNS * EVENTSPERRUN)) events
+##### xqcut value of 25
+####################################################################################
+define l+ = e+ mu+ ta+ 
+define l- = e- mu- ta-
+generate  p p > w+ w- @0
+add process p p > w+ w- j @1
+add process p p > w+ w- j j @2
+output ${FILE}
+launch ${FILE} -i
+multi_run ${BACKGROUNDRUNS}
+1
+4
+0
+set ebeam = 6500
+set nevents = ${EVENTSPERRUN}
+set ickkw = 1
+set xqcut = 25
+set etaj = 5
+decay w+ > l+ vl
+decay w- > l- vl~
+0
+EOM
+
+cd $WORKINGPATH
+
+sudo docker cp $OUTPUT/MADGraphScripts/$FILE MADGraphDocker:/var/MG_outputs
+
+sudo docker exec -it MADGraphDocker /home/hep/MG5_aMC_v2_6_3_2/bin/mg5_aMC  $FILE
+
+#############################################################
+### End of process generator  ###############################
+#############################################################	
+
+################################## Signal Events ############
+
+for SMUONMASS in 200 400
+do	
+FILE="PPtoSmuonSmuon_Smuon_Mass_${SMUONMASS}"
+
+/bin/cat <<EOM >$FILE
+####################################################################################
+##### File Generates the following
+##### Proton Proton to smuon pair with two jets (These smuons decay to muons) 
+##### With $((SIGNALRUNS * EVENTSPERRUN)) events
+##### xqcut value of 55
+####################################################################################
+
+
+import model MSSM_UFO/
+generate p p > mur- mur+ @0
+add process p p > mur- mur+ j @1
+add process p p > mur- mur+ j j @2
+output ${FILE}
+launch ${FILE} -i
+multi_run ${SIGNALRUNS}
+1
+4
+0
+decay mur- > mu- n1
+decay mur+ > mu+ n1
+set ebeam = 6500
+set nevents = ${EVENTSPERRUN}
+set ickkw = 1
+set xqcut = 55
+set etaj = 5
+0
+EOM
+
+sudo docker cp $OUTPUT/MADGraphScripts/$FILE MADGraphDocker:/var/MG_outputs
+
+sudo docker exec -it MADGraphDocker /home/hep/MG5_aMC_v2_6_3_2/bin/mg5_aMC  $FILE
+
+#############################################################
+### End of process generator  ###############################
+#############################################################
+done
 
 sudo docker kill MADGraphDocker
 
